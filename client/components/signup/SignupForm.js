ae541d69c07a9fbcc1c0b646b1721f2d32104674
Andre Imano <andreseed@gmail.com>
2017-12-14 13:46:49 -0200

    Signup Server Side

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -1,7 +1,8 @@
 import React from "react";
 import timezones from "../../data/timezones";
 import map from "lodash/map";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import classnames from "classnames";
 
 class SignupForm extends React.Component {
   constructor(props) {
@@ -10,7 +11,9 @@ class SignupForm extends React.Component {
       username: "",
       email: "",
       password: "",
-      passwordConfirmation: ""
+      passwordConfirmation: "",
+      errors: {},
+      isLoading: false
     };
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
@@ -22,90 +25,80 @@ class SignupForm extends React.Component {
 
   onSubmit(e) {
     e.preventDefault();
-    //axios.post('/api/users', { user: this.state });
-    this.props.userSignupRequest(this.state);
+    this.setState({ errors: {}, isLoading:true });
+    this.props
+      .userSignupRequest(this.state)
+        .then(() => {}, ({ data }) => this.setState({ errors: data, isLoading: false }));
   }
 
   render() {
+    const { errors } = this.state;
     const options = map(timezones, (val, key) => (
       <option key={val} value={val}>
         {key}
       </option>
     ));
 
-    return (
-      <form onSubmit={this.onSubmit}>
+    return <form onSubmit={this.onSubmit}>
         <h1>Entre com seu login:</h1>
 
-        <div className="form-group">
+        <div className={classnames("form-group", {
+            "has-error": errors.username
+          })}>
           <label className="control-label">Usuário</label>
-          <input
-            value={this.state.username}
-            onChange={this.onChange}
-            type="text"
-            name="username"
-            className="form-control"
-          />
+          <input value={this.state.username} onChange={this.onChange} type="text" name="username" className="form-control" />
+          {errors.username && <span className="help-block">
+              {errors.username}
+            </span>}
         </div>
 
-        <div className="form-group">
+        <div className={classnames("form-group", {
+            "has-error": errors.email
+          })}>
           <label className="control-label">E-mail</label>
-          <input
-            value={this.state.email}
-            onChange={this.onChange}
-            type="text"
-            name="email"
-            className="form-control"
-          />
+          <input value={this.state.email} onChange={this.onChange} type="text" name="email" className="form-control" />
+          {errors.email && <span className="help-block">
+              {errors.email}
+            </span>}
         </div>
 
-        <div className="form-group">
+        <div className={classnames("form-group", {
+            "has-error": errors.password
+          })}>
           <label className="control-label">Senha</label>
-          <input
-            value={this.state.password}
-            onChange={this.onChange}
-            type="password"
-            name="password"
-            className="form-control"
-          />
+          <input value={this.state.password} onChange={this.onChange} type="password" name="password" className="form-control" />
+          {errors.password && <span className="help-block">
+              {errors.password}
+            </span>}
         </div>
 
-        <div className="form-group">
+        <div className={classnames("form-group", {
+            "has-error": errors.passwordConfirmation
+          })}>
           <label className="control-label">Confirme a Senha</label>
-          <input
-            value={this.state.passwordConfirmation}
-            onChange={this.onChange}
-            type="password"
-            name="passwordConfirmation"
-            className="form-control"
-          />
+          <input value={this.state.passwordConfirmation} onChange={this.onChange} type="password" name="passwordConfirmation" className="form-control" />
+          {errors.passwordConfirmation && <span className="help-block">
+              {errors.passwordConfirmation}
+            </span>}
         </div>
 
         <div className="form-group">
           <label className="control-label">Timezone</label>
-          <select
-            className="form-control"
-            name="timezone"
-            onChange={this.onChange}
-            value={this.state.timezone}
-          >
-            <option value="" >
-              Escolha seu fuso horário
-            </option>
+          <select className="form-control" name="timezone" onChange={this.onChange} value={this.state.timezone}>
+            <option value="">Escolha seu fuso horário</option>
             {options}
           </select>
         </div>
 
         <div className="form-group">
-          <button className="btn btn-primary btn-lg">Entrar</button>
+          <button disabled={this.state.isLoading} className="btn btn-primary btn-lg">Entrar</button>
         </div>
-      </form>
-    );
+      </form>;
   }
 }
 
 SignupForm.propTypes = {
-    userSignupRequest: PropTypes.func.isRequired //React.PropTypes.func.isRequired
-}
+  userSignupRequest: PropTypes.func.isRequired
+};
 
 export default SignupForm;
--- server/index.js
+++ server/index.js
@@ -1,25 +1,33 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
+import bodyParser from "body-parser";
 
-import webpack from 'webpack';
-import webpackMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import webpackConfig from '../webpack.config.dev';
+import webpack from "webpack";
+import webpackMiddleware from "webpack-dev-middleware";
+import webpackHotMiddleware from "webpack-hot-middleware";
+import webpackConfig from "../webpack.config.dev";
+
+import users from "./routes/users";
 
 let app = express();
 
-const compiler = webpack(webpackConfig);
+// User Sign Up - Server-side validation
+app.use(bodyParser.json());
+app.use("/api/users", users);
 
-app.use(webpackMiddleware(compiler, {
+//Hot automatic compiler
+const compiler = webpack(webpackConfig);
+app.use(
+  webpackMiddleware(compiler, {
     hot: true,
     publicPath: webpackConfig.output.publicPath,
     noInfo: true
-}));
-
+  })
+);
 app.use(webpackHotMiddleware(compiler));
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
 });
 
-app.listen(3000, () => console.log('Running on 192.168.0.18:3000'));
+app.listen(3000, () => console.log("Running on 192.168.0.18:3000"));
